4f6de7eeebb2c382c980c41284153f1587fa82df
Ludovic Vendette <117288936+Loudo7000@users.noreply.github.com>
2023-04-25 15:52:31 -0400

    player mouvement

---
+++ Assets/_MyAssets/Scripts/Player.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+
+    [SerializeField] private float _speed = 1.6f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MouvementJoueur();
+    }
+
+    private void MouvementJoueur()
+    {
+        float posHorizontal = Input.GetAxis("Horizontal");
+        float posVertical = Input.GetAxis("Vertical");
+
+        Vector3 direction = new Vector3(posHorizontal, posVertical, 0f);
+
+        direction.Normalize();
+
+        transform.Translate(direction * Time.deltaTime * _speed);
+
+        transform.position = new Vector3(Mathf.Clamp(transform.position.x, -4.26f, 4.26f), Mathf.Clamp(transform.position.y, -3.38f, 4.12f), 0f);
+
+        if (transform.position.x >= 9.5f)
+        {
+            transform.position = new Vector3(-9.5f, transform.position.y, 0f);
+        }
+        else if (transform.position.x <= -9.5f)
+        {
+            transform.position = new Vector3(9.5f, transform.position.y, 0f);
+        }
+    }
+}
