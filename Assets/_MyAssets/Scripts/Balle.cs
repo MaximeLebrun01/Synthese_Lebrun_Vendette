8de7b3fe7161428b97d8082010e2b5def509c83e
Ludovic Vendette <117288936+Loudo7000@users.noreply.github.com>
2023-04-28 20:11:20 -0400

    fire

--- Assets/_MyAssets/Scripts/Balle.cs
+++ Assets/_MyAssets/Scripts/Balle.cs
@@ -5,18 +5,40 @@ using UnityEngine;
 public class Balle : MonoBehaviour
 {
 
-    [SerializeField] private float _vitesse = 10f;
+    float inputHorizontal;
+    float inputVertical;
+
+    [SerializeField] private float _vitesse = 5f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        inputHorizontal = Input.GetAxis("Fire1");
+        inputVertical = Input.GetAxis("Fire2");
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.right * Time.deltaTime * _vitesse);
-        if (transform.position.x > 9f)
+
+        if (inputHorizontal != 0 || inputVertical != 0)
+        {
+            transform.Translate(new Vector3(inputHorizontal, inputVertical, 0f) * Time.deltaTime * _vitesse);
+
+        }
+
+        if (transform.position.x > 4.5f)
+        {
+            Destroy(gameObject);
+        }
+        else if (transform.position.x < -4.5f)
+        {
+            Destroy(gameObject);
+        }
+        else if (transform.position.y > 4f)
+        {
+            Destroy(gameObject);
+        }
+        else if (transform.position.y < -4f)
         {
             Destroy(gameObject);
         }
--- Assets/_MyAssets/Scripts/Player.cs
+++ Assets/_MyAssets/Scripts/Player.cs
@@ -6,16 +6,22 @@ public class Player : MonoBehaviour
 {
 
 
-    [SerializeField] private float _speed = 1.6f;
+    [SerializeField] private float _speed = 2f;
     [SerializeField] private GameObject _balle = default;
+    [SerializeField] private float _fireRate = 0.5f;
+
+    private float _canfire = -1f;
+    private float _CandenceInitial = -1f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        _CandenceInitial = _fireRate;
+
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         MouvementJoueur();
         Tir();
@@ -23,13 +29,16 @@ public class Player : MonoBehaviour
 
     private void Tir()
     {
-        float inputHorizontal = Input.GetAxis("Fire1");
-        float inputVertical = Input.GetAxis("Fire2");
-        if(Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.LeftArrow))
+        if (Time.time > _canfire)
         {
-            Instantiate(_balle, transform.position + new Vector3(inputHorizontal, 0f, 0f), Quaternion.identity);
+            _canfire = Time.time + _fireRate;
+            float inputHorizontal = Input.GetAxis("Fire1");
+            float inputVertical = Input.GetAxis("Fire2");
+            if (inputHorizontal != 0 || inputVertical != 0)
+            {
+                Instantiate(_balle, transform.position, Quaternion.identity);
+            }
         }
-
         
     }
 
@@ -39,8 +48,6 @@ public class Player : MonoBehaviour
         float posVertical = Input.GetAxis("Vertical");
 
         Vector3 direction = new Vector3(posHorizontal, posVertical, 0f);
-
-        direction.Normalize();
 
         transform.Translate(direction * Time.deltaTime * _speed);
 
